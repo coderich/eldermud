6796fbdbff9ab2650214b15e89f2ad823235f4c0
Richard Livolsi <richard.livolsi@goziohealth.com>
2019-06-24 16:05:09 -0400

    Refactor; who knows

--- server/src/server.js
+++ server/src/server.js
@@ -2,18 +2,21 @@ import SocketServer from 'socket.io';
 import RequireDir from 'require-dir';
 import dao from './dao';
 
+const mapValue = (value, baseName) => {
+  const [fn = value] = Object.values(value);
+  return fn;
+};
+
 // Setup Server
 const server = new SocketServer(3003, { serveClient: false, pingTimeout: 30000 });
 
 // Modules
-const modules = RequireDir('./module', {
-  recurse: true,
-  mapValue: (value, baseName) => {
-    const [fn = value] = Object.values(value);
-    return fn;
-  },
+Object.entries(RequireDir('./module', { recurse: true, mapValue })).forEach(([name, fn]) => {
+  const mod = fn(dao.addStoreModel(name));
+  dao.store.loadModule(name, mod);
 });
 
-Object.values(modules).forEach(fn => fn(server, dao));
+// Listeners
+Object.values(RequireDir('./listener')).forEach(fn => fn(server, dao));
 
 export default server;
