bdf29d251840c0187f8f44a2c7e306208f60b3f2
Richard Livolsi <richard.livolsi@goziohealth.com>
2025-06-19 11:53:50 -0400

    Updated status and room gmcp

--- engine/src/module/info/status.js
+++ engine/src/module/info/status.js
@@ -3,8 +3,6 @@ const { Action } = require('@coderich/gameflow');
 Action.define('status', [
   async (_, { actor }) => {
     const status = await actor.mGet('hp', 'ma', 'exp', 'posture');
-    status.mhp = actor.mhp;
-    status.mma = actor.mma;
     status.posture += 'ing';
     actor.send('status', status);
   },
